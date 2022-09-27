21f5cd5d3b253400c14b03effeee79cf63d2901b
GreenMechanoid <daniel.svensson682@gmail.com>
2022-09-27 12:04:01 +0200

    Before Lunch stuff

--- Övningar_27_09_22/Program.cs
+++ Övningar_27_09_22/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int Count = 0;
+            string UserInput = "";
+            Console.WriteLine("Please enter a number");
+            Count = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter segments");
+            string InputSegments = Console.ReadLine();
+            int NumSegments = Int32.Parse(InputSegments);
+            for (int segment = 1; segment <= NumSegments; segment++)
+            {
+                for (int row = segment * NumSegments - NumSegments; row <= Count; row++)
+                {
+
+                    for (int squiggly = 1; squiggly <= Count - row; squiggly++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int collumn = 1; collumn <= (row * 2 ) -1; collumn++)
+                    {
+                        Console.Write("*");
+                    }
+                    for (int squiggly = 1; squiggly <= Count - row; squiggly++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            //while (count < 5)
+            //{
+            //    Console.WriteLine("*");
+            //    count++;
+            //}
+
+
+
+
         }
     }
 }
