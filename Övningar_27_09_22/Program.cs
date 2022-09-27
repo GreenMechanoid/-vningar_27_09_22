299f936cb067940cc5d1fcdaf5bc06f54c5cf5f8
GreenMechanoid <daniel.svensson682@gmail.com>
2022-09-27 10:14:36 +0200

    Add project files.

---
+++ Övningar_27_09_22/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Övningar_27_09_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
